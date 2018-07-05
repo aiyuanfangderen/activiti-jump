90a1b951ebc817b879d82ec9047f2ade98b959b0
XFour <837065279@qq.com>
2018-07-05 21:25:27 +0800

    0705修改

--- src/main/java/demo/controller/LoginController.java
+++ src/main/java/demo/controller/LoginController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
