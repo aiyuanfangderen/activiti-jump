8bb3bb30bb791d52b43ea2a5980bc671079a26af
XFour <837065279@qq.com>
2018-07-08 13:12:26 +0800

    0708

---
+++ src/main/java/demo/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Author:许清远
+ * Data:2018/7/8
+ * Description:
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
+
